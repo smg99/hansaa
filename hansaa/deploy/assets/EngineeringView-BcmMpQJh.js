ffcf8f7edbd3186425f0a4e9b0842d75ad83fcb9
Sumit <sumitg3767@gmail.com>
2024-09-18 12:10:28 +0530

    publish depoy

---
+++ hansaa/deploy/assets/EngineeringView-BcmMpQJh.js
@@ -0,0 +1,8 @@
+import{E as t}from"./Element-D2IJvBKK.js";import{_ as r,c as i,r as o,o as s}from"./index-DEf_SE5m.js";const a={components:{Element:t},data(){return{headerText:"Innovative engineering",subHeaderText:`Our extensive experience in cover solutions, <br />
+gives you the benefits of:`,paragraphText:`- Advanced in-house engineering <br/>
+                      - Innovative tailored design <br/>
+                      - Seamless integration from idea to
+                       serial manufacturing <br/>
+                      - Own production facilities worldwide <br/>
+                      - Trusted Partner <br/>
+                      - Efficient global delivery`,images:[{src:"set3/Trappe2.jpg",alt:"Engineering Solution"},{src:"set3/HubOnDeck_HansAa.jpg",alt:"Engineering Solution"},{src:"set3/HUB5.JPG",alt:"Engineering Solution"},{src:"set3/HUB9.jpeg",alt:"Engineering Solution"},{src:"set3/NACELLE1.JPG",alt:"Engineering Solution"},{src:"set3/ENGINEERING6.jpg",alt:"Engineering Solution"},{src:"set3/HUB.JPG",alt:"Engineering Solution"},{src:"set3/BLADE3.JPG",alt:"Engineering Solution"},{src:"set3/TOWER.JPG",alt:"Engineering Solution"},{src:"set3/vestas tower.JPG",alt:"Engineering Solution"}]}},methods:{}};function g(l,c,p,u,e,d){const n=o("Element");return s(),i(n,{headerText:e.headerText,subHeaderText:e.subHeaderText,paragraphText:e.paragraphText,images:e.images},null,8,["headerText","subHeaderText","paragraphText","images"])}const x=r(a,[["render",g],["__scopeId","data-v-7f3a9d8e"]]);export{x as default};
--- hansaa/src/views/HomeView.vue
+++ hansaa/src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main style="display: flex; align-items: center; justify-content: center; align-content: center;">
+    Frontpage coming soon... <br />
+    <RouterLink to="/engineering">Engineering</RouterLink><br />
+    <RouterLink to="/tower">Tower</RouterLink><br />
+    <RouterLink to="/blade">Blades</RouterLink><br />
+    <RouterLink to="/hub">Hubs</RouterLink><br />
+    <RouterLink to="/nacelle">Nacelle</RouterLink>
   </main>
 </template>
