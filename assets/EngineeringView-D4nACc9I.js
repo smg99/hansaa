754f838c8e79543c5912ab3dbd937ddbefe0b5fb
Sumit <sumitg3767@gmail.com>
2024-09-19 17:31:47 +0530

    new deploy

--- assets/EngineeringView-D4nACc9I.js
+++
@@ -1,8 +0,0 @@
-import{E as t}from"./Element-B3yY9MVa.js";import{_ as r,c as i,r as o,o as s}from"./index-Bzj9e-3A.js";import"./carousel.es-C8sFVLes.js";const a={components:{Element:t},data(){return{headerText:"Innovative engineering",subHeaderText:`Our extensive experience in cover solutions, <br />
-      gives you the benefits of:`,paragraphText:`- Advanced in-house engineering <br/>
-                      - Innovative tailored design <br/>
-                      - Seamless integration from idea to
-                       serial manufacturing <br/>
-                      - Own production facilities worldwide <br/>
-                      - Trusted Partner <br/>
-                      - Efficient global delivery`,images:[{src:"set3/Trappe2.jpg",alt:"Engineering Solution"},{src:"set3/HubOnDeck_HansAa.jpg",alt:"Engineering Solution"},{src:"set3/HUB5.JPG",alt:"Engineering Solution"},{src:"set3/HUB9.jpeg",alt:"Engineering Solution"},{src:"set3/NACELLE1.JPG",alt:"Engineering Solution"},{src:"set3/ENGINEERING6.jpg",alt:"Engineering Solution"},{src:"set3/HUB.JPG",alt:"Engineering Solution"},{src:"set3/BLADE3.JPG",alt:"Engineering Solution"},{src:"set3/TOWER.JPG",alt:"Engineering Solution"},{src:"set3/vestas tower.JPG",alt:"Engineering Solution"}]}},methods:{}};function g(l,c,p,u,e,E){const n=o("Element");return s(),i(n,{headerText:e.headerText,subHeaderText:e.subHeaderText,paragraphText:e.paragraphText,images:e.images},null,8,["headerText","subHeaderText","paragraphText","images"])}const T=r(a,[["render",g],["__scopeId","data-v-8a61a928"]]);export{T as default};
